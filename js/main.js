91f5ccd8b935587826b1e54224b1589d224fc41c
Emil Bruun Larsen <60384288+Emilovich98@users.noreply.github.com>
2020-09-17 10:11:23 +0200

    Keywords array and platfrom array works

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ let _programs = [];
 // ========== READ ==========
 // watch the database ref for changes
 programsRef.onSnapshot(function (snapshotData) {
-_programs = [];
+    _programs = [];
     snapshotData.forEach(function (doc) {
         //console.log(doc);
         let program = doc.data();
@@ -36,32 +36,29 @@ function searchPrograms(value) {
     console.log(value);
     let filteredPrograms = [];
     for (let program of _programs) {
-      let companyName = program.companyName.toLowerCase();
-      let name = program.name.toLowerCase();
-      let price = program.price.toLowerCase();
-      let keywordsOne = program.price.toLowerCase();
-      //let keywords = program.keywords.chilld.toLowerCase()
-      if (companyName.includes(value.toLowerCase()))
-      {
-        filteredPrograms.push(program);
-      }
-      else if (name.includes(value.toLowerCase()))
-      {
-        filteredPrograms.push(program);
-      }
-      else if (price.includes(value.toLowerCase()))
-      {
-        filteredPrograms.push(program);
-      }
-      /*lse if (keywords.includes(value.toLowerCase()))
-      {
-        filteredPrograms.push(program);
-      }*/
+        let companyName = program.companyName.toLowerCase();
+        let name = program.name.toLowerCase();
+        let price = program.price.toLowerCase();
+        let keywordsOne = program.price.toLowerCase();
+        //let keywords = program.keywords.chilld.toLowerCase()
+        if (companyName.includes(value.toLowerCase())) {
+            filteredPrograms.push(program);
+        }
+        else if (name.includes(value.toLowerCase())) {
+            filteredPrograms.push(program);
+        }
+        else if (price.includes(value.toLowerCase())) {
+            filteredPrograms.push(program);
+        }
+        /*lse if (keywords.includes(value.toLowerCase()))
+        {
+          filteredPrograms.push(program);
+        }*/
     }
-  
+
     console.log(filteredPrograms);
     appendPrograms(filteredPrograms);
-  }
+}
 
 
 function appendKeyword(_keyword) {
@@ -128,7 +125,7 @@ function appendPrograms(programs) {
                                 <div class="filter-box">
                                     <h4>Platform</h4>
                                     <div class="platform-container">
-                                        <img src="${program.platform}" alt="Platform">
+                                        ${platformsArray(program.platform)}
                                     </div>
                                 </div>
                             </div>
@@ -143,7 +140,7 @@ function appendPrograms(programs) {
                     <hr class="line-devider">
                     <div class="card-bottum-container">
                         <div class="card-keywords-container">
-                            <p class="keyword">${program.keywords}</p>
+                            ${keywordsArray(program.keywords)}
                         </div>
                         <div class="course-btn-container">
                             <a class="course-btn" target="_blank" href="${program.course}">Start YouTube course</a>
@@ -155,3 +152,22 @@ function appendPrograms(programs) {
     document.querySelector('#content-area').innerHTML = htmlTemplate;
 };
 
+function keywordsArray(keywords) {
+    let template = "";
+    for (const keyword of keywords) {
+        template += /*html*/ `
+             <p class="keyword">${keyword}</p>
+        `;
+    }
+    return template;
+}
+
+function platformsArray(platforms) {
+    let template = "";
+    for (const platform of platforms) {
+        template += /*html*/ `
+             <img src="${platform}" alt="Platform">
+        `;
+    }
+    return template;
+}
